650c7f021a18fa6c6f3999f6f3e97f22747c3e25
Julian Kang <julianswkang@gmail.com>
2022-05-23 18:56:30 -0400

    created index.js file with root, starting app.jsx file

---
+++ client/containers/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const App = () => {
+
+}
+
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
+import App from './containers/app.jsx'
 
-const root = createRoot
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />)
