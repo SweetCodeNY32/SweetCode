3c87ad123a18ad2feda6c36e3c9082cf4da1a7ca
Emilia Yoffie <emiliayoffie@gmail.com>
2022-05-24 00:10:11 -0400

    fix errors, add to server, add dependencies

--- server/server.js
+++ server/server.js
@@ -5,3 +5,8 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config
 
 const authController = require('./controllers/authController');
+const { append } = require('express/lib/response');
+
+app.get('/api/github-callback', authController.getToken, authController.getUser, (req, res) => {
+    return res.status(200); //should I send back any data ie res.locals
+})
