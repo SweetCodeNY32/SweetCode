7a99b9d1a6cb853dc0bafcaeecbaa529c913ca3c
Julian Kang <julianswkang@gmail.com>
2022-05-24 12:10:18 -0400

    moved the sidebar into components file. change app file to only render the home container if user is logged in

--- client/containers/app.jsx
+++ client/containers/app.jsx
@@ -1,21 +1,25 @@
 import React, {useState} from 'react';
+import Home from './home.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-
+//the main application container
 const App = () => {
+  //will hold logic for react hooks to manage username and setting username
   const [username, setUsername] = useState('');
 
-
+  //if user is not currently signed in, will render the login page
+  //login component will have access to setusername 
   if (username === ''){
     return (
-      <Login />
+      <Login setUsername={setUsername}/>
     );
   }
 
+  //once user is logged in, will render the sidebar and study guide pages
+  //will pass in the username into the sidebar to provide personality to the sidebar
   else return(
     <div class="logged-in">
-      <Sidebar />
-      <StudyGuide />
+      <Home username={username}/>
     </div>
 
   );
--- client/containers/login.jsx
+++ client/containers/login.jsx
@@ -1,18 +1,30 @@
- import React from 'react';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 
 
+const Login = (props) => {
 
- const Login = (props) => {
 
-  
+  async function handleLogin(){
+    let response = axios.get('/auth/login');
+    let username = response.username;
+
+    props.setUsername(username);
+  }
 
   return(
-    <div class="">
+    <div class="login">
+      <h1>Sweet Code</h1>
+
+      <Button
+        onClick={handleLogin}
+        variant="outlined"
+      >Log in with Github here!</Button>
 
     </div>
   )
 
- }
+}
 
- export default Login
+export default Login
