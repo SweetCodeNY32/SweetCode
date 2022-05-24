752ef771e60ec240322d862b5c5ae8d072641662
Julian Kang <julianswkang@gmail.com>
2022-05-24 10:30:32 -0400

    finished addcategory component, working on autocomplete logic for addquestion

--- client/components/addCategory.jsx
+++ client/components/addCategory.jsx
@@ -1,2 +1,19 @@
 //component used to add categories to the study guide
 //will have imput field and add category button
+
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const AddCategory = (props) => {
+
+
+  return(
+    <div class="add-category">
+      <TextField variant="outlined" label="Add category here"></TextField>
+      <Button variant="outlined">Add Category</Button>
+    </div>
+  )
+}
+
+export default AddCategory;
--- client/components/addQuestion.jsx
+++ client/components/addQuestion.jsx
@@ -1,2 +1,20 @@
 //component used to add questions to a specific category
 //will have input text field and add question button
+
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const AddQuestion = (props) => {
+
+
+  return(
+    <div class="add-question">
+      <TextField variant="outlined" label="Add question here"></TextField>
+      <Button variant="outlined">Add Question</Button>
+    </div>
+  )
+}
+
+export default AddQuestion;
